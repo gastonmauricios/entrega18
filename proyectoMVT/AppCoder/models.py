494b0f4a3e7be3c29801dcb725f2778f4ab2adbc
gastonmauricios <gastonmauricios@gmail.com>
2022-12-14 14:32:34 -0300

    familiares

--- proyectoMVT/AppCoder/models.py
+++ proyectoMVT/AppCoder/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here. clase curso
 
-class Curso(models.Model):
-    nombre=models.CharField(max_length=50) # es el str
-    comision=models.IntegerField()
+class Familiares(models.Model):
+    nombre=models.CharField(max_length=20) # es el str
+    tipoFamilia=models.IntegerField()
+    edad=models.IntegerField(max_length=2)
 
     def __str__(self):
-        return self.nombre+" "+str(self.comision)
+        return self.nombre+" "+str(self.tipoFamilia)+" "+str(self.edad)
