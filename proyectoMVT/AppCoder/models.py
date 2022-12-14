1810884497fe4de8f98d129137bb1abdbb4441ce
gastonmauricios <gastonmauricios@gmail.com>
2022-12-14 15:14:23 -0300

    familiares mama papa hermana

--- proyectoMVT/AppCoder/models.py
+++ proyectoMVT/AppCoder/models.py
@@ -6,22 +6,22 @@ class Padre(models.Model):
     papa=models.CharField(max_length=20)
     nombre=models.CharField(max_length=20) # es el str #campo de texto caracteres
     apellido=models.CharField(max_length=20)
-    edad=models.IntegerField()
-    profesion=models.CharField()
+    naciemiento=models.DateField()   
+    edad=models.BooleanField()
 
     def __str__(self):
-        return self.nombre+" "+(self.apellido)+" "+str(self.edad)
+        return self.nombre+" "+(self.apellido)+" "+str(self.edad)+" "+str(self.papa)
 
 class Mama(models.Model):
     mama=models.CharField(max_length=20)
     nombre=models.CharField(max_length=20) # es el str
     apellido=models.CharField(max_length=20)
-    edad=models.IntegerField()    
-    profesion=models.CharField()
+    naciemiento=models.DateField()    
+    edad=models.BooleanField()
 
 class Hermana(models.Model):
     hermana=models.CharField(max_length=20)
     nombre=models.CharField(max_length=20) # es el str
     apellido=models.CharField(max_length=20)
-    edad=models.IntegerField()
-    profesion=models.CharField()
+    naciemiento=models.DateField()   
+    edad=models.BooleanField()
