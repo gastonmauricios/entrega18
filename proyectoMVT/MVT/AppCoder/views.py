9d93aed0f0f1fa629c1eb7a8d128813e3406e512
gastonmauricios <gastonmauricios@gmail.com>
2022-12-14 16:32:10 -0300

    familiares mama papa hermana

--- proyectoMVT/MVT/AppCoder/views.py
+++ proyectoMVT/MVT/AppCoder/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Papa
+from django.http import HttpResponse
 
 # Create your views here.
+def familia(request):
+
+    familiares= Papa(nombre="Nicolas", apellido="Sciarra")
+    familiares.save()
+    cadena_texto=f"famialia guardada: Nombre: {familiares.nombre}"
+    
+    return HttpResponse(cadena_texto)
+
+
