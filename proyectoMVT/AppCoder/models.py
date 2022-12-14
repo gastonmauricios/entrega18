5c3e989bb579830580c6400d171f3cc1f44922a5
gastonmauricios <gastonmauricios@gmail.com>
2022-12-14 14:59:39 -0300

    familiares mama papa hermana

--- proyectoMVT/AppCoder/models.py
+++ proyectoMVT/AppCoder/models.py
@@ -2,12 +2,26 @@ from django.db import models
 
 # Create your models here. clase curso
 
-class Familiares(models.Model):
-    nombre=models.CharField(max_length=20) # es el str
-    apellido=models.IntegerField()
-    edad=models.IntegerField(max_length=2)
+class Padre(models.Model):
+    papa=models.CharField(max_length=20)
+    nombre=models.CharField(max_length=20) # es el str #campo de texto caracteres
+    apellido=models.CharField(max_length=20)
+    edad=models.IntegerField()
+    profesion=models.CharField()
 
     def __str__(self):
         return self.nombre+" "+(self.apellido)+" "+str(self.edad)
 
+class Mama(models.Model):
+    mama=models.CharField(max_length=20)
+    nombre=models.CharField(max_length=20) # es el str
+    apellido=models.CharField(max_length=20)
+    edad=models.IntegerField()    
+    profesion=models.CharField()
+
 class Hermana(models.Model):
+    hermana=models.CharField(max_length=20)
+    nombre=models.CharField(max_length=20) # es el str
+    apellido=models.CharField(max_length=20)
+    edad=models.IntegerField()
+    profesion=models.CharField()
