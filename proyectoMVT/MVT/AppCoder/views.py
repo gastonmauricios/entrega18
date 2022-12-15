aa19d37db124442e1b3d786bfa4948418eac2647
gastonmauricios <gastonmauricios@gmail.com>
2022-12-15 16:51:26 -0300

    mdelos madre herman

--- proyectoMVT/MVT/AppCoder/views.py
+++ proyectoMVT/MVT/AppCoder/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Papa
+from .models import Papa, Madre, Hermana
 from django.http import HttpResponse 
 
 # Create your views here. controladores en los views
@@ -10,4 +10,16 @@ def papa(request):
     papitos.save()
     cadena_texto=f"Nombre de familiares: Nombre: {papitos.nombre}, Apellido: {papitos.apellido}, Edad {papitos.edad}, Fecha de Nac: {papitos.nacimiento}"
     return HttpResponse(cadena_texto)
+
     
+def mama(request):
+    mamitas= Madre (nombre="Mirta", apellido="Riquelme", edad=70, nacimiento="30 de enero 1948")
+    mamitas.save()
+    cadena_texto=f"Nombre de familiares: Nombre: {mamitas.nombre}, Apellido: {mamitas.apellido}, Edad {mamitas.edad}, Fecha de Nac: {mamitas.nacimiento}"
+    return HttpResponse(cadena_texto)
+
+def hermana(request):
+    hermanita= Hermana (nombre="Caro", apellido="Sciarra", edad=74, nacimiento="26 de septiembre")
+    hermanita.save()
+    cadena_texto=f"Nombre de familiares: Nombre: {hermanita.nombre}, Apellido: {hermanita.apellido}, Edad {hermanita.edad}, Fecha de Nac: {hermanita.nacimiento}"
+    return HttpResponse(cadena_texto)
